79088b56d6eff2ab63621a65acb40045ef4ae165
BrendenKingTLG <brenden.king@tlgcohort.com>
2022-09-06 11:56:35 -0600

    applied constructors v1

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,20 +1,61 @@
 
 public class Television {
-  //instance variables
-  String brand;
-  int volume;
-  boolean on;
 
-  //methods
+  //instance variables
+  private String brand;
+  private int volume;
+
+
+  //constructor
+  public Television(String brand) {
+    setBrand(brand);
+  }
+
+  public Television(String brand, int volume){
+    this(brand);
+    setVolume(volume);
+  }
+
+
+  //business methods
   void turnOn(){
-    System.out.println("turning on " + brand + " tv");
-    on = true;
+    boolean isConnected = verifyInternetConnection();
+    System.out.println("turning on " + getBrand() + " tv" + "at volume " + getVolume());
+
   }
 
   void turnOff(){
-    System.out.println("turning off " + brand + " tv");
-    on = false;
+    System.out.println("turning off " + getBrand() + " tv"+ "at volume " + getVolume());
+
   }
+
+  private boolean verifyInternetConnection() {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Television " + "brand " + getBrand() + " , " + "volume " + getVolume();
+  }
+
+
+  //getters & setters
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
 
   }
 
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -2,26 +2,28 @@ public class TelevisionClient {
 
   public static void main(String[] args) {
       //create first instance
-      Television tv1 = new Television();
+      Television tv1 = new Television("Onn", 20);
 
       //change default values
-      tv1.brand = "Samsung";
-      tv1.volume = 10;
+      //tv1.setBrand("Samsung");
+     // tv1.setVolume(10);
+
 
       //check methods
       tv1.turnOn();
       tv1.turnOff();
 
       //create second instance
-      Television tv2 = new Television();
+      Television tv2 = new Television("LG", 50);
 
       //change default values
-      tv2.brand = "LG";
-      tv2.volume = 8;
+      // tv2.setBrand("LG");
+      // tv2.setVolume(10);
 
       //check methods
       tv2.turnOn();
       tv2.turnOff();
+
 
   }
 }
