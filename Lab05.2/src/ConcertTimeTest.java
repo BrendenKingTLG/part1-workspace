bbe4ac36d41ae85d0017a8d53240cd377db43cf7
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-09-12 18:55:59 -0700

    completed lab 5.2

--- Lab05.2/src/ConcertTimeTest.java
+++ Lab05.2/src/ConcertTimeTest.java
@@ -13,13 +13,13 @@ class ConcertTimeTest {
 
     public static void main(String[] args) {
         listAvailableZoneIds();
-        // testLiveConcert();
+        testLiveConcert();
     }
 
-    /*
-     * Shows all defined ZoneIds, which will help with the concert problem.
-     * You'll need to know the names of the ZoneIds for Toronto and Perth.
-     */
+
+//      Shows all defined ZoneIds, which will help with the concert problem.
+//      You'll need to know the names of the ZoneIds for Toronto and Perth.
+
     public static void listAvailableZoneIds() {
         Set<String> rawZoneIds = ZoneId.getAvailableZoneIds();  // unsorted, arbitrary order
         SortedSet<String> zoneIds = new TreeSet<>(rawZoneIds);  // sorted by String natural order
@@ -29,15 +29,13 @@ class ConcertTimeTest {
         }
     }
 
-    /**
-     * OPTIONAL: your favorite band is playing live in Toronto, Canada on June 18, 2020, at 7pm.
-     * You live in Perth, Australia, and want to watch a live simulcast via the web.
-     * When are you watching it?
-     *
-     * RESULT: 
-     */
+
+//      OPTIONAL: your favorite band is playing live in Toronto, Canada on June 18, 2020, at 7pm.
+//      You live in Perth, Australia, and want to watch a live simulcast via the web.
+//      When are you watching it?
+//      RESULT:
+
     public static void testLiveConcert() {
-        // TODO
         ZonedDateTime concertTime = ZonedDateTime.of(LocalDateTime.of(2020, 6, 18, 19, 0), ZoneId.of("Canada/Eastern"));
         ZonedDateTime perthTime = concertTime.withZoneSameInstant(ZoneId.of("Australia/Perth"));
 
